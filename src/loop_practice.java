dd8f2ad7a5601cd0b0d20012141b7105fffab1a2
shalakabibikar <shalaka.bibikar@gmail.com>
2025-01-26 16:06:38 +0530

    first commit

---
+++ src/loop_practice.java
@@ -0,0 +1,14 @@
+public class loop_practice {
+    public static void main(String[] args) {
+        int n=4;
+        System.out.println("For loop practice set:");
+        for (int i=n; i>0; i--){
+            for (int j = 0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            System.out.println("\n");
+        }
+
+    }
+}
